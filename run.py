99d9915c11d8d5d871250748129f23df898c5c93
FIK001 <samuelfikayomi08@gmail.com>
2025-10-29 16:34:05 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
-from src.app import app, create_tables
+from src.app import app
+from src.db import create_tables  # <-- correct import
 
 if __name__ == "__main__":
     create_tables()  # Ensure database tables exist
